a2305eb95a4b6f244f20f90f7beb44271f2e87ba
Milica998 <babicka51@gmail.com>
2020-12-24 21:13:45 +0100

    Creating new section and adding swiper

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,40 @@
+function objectFitPolyfill() {
+	if ('objectFit' in document.documentElement.style === false) {
+		document.addEventListener('DOMContentLoaded', function () {
+			Array.prototype.forEach.call(document.querySelectorAll('img[data-object-fit]'), function (image) {
+				(image.runtimeStyle || image.style).background = 'url("' + image.src + '") no-repeat 50%/' + (image.currentStyle ? image.currentStyle['object-fit'] : image.getAttribute('data-object-fit'));
+	
+				image.src = 'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'' + image.width + '\' height=\'' + image.height + '\'%3E%3C/svg%3E';
+			});
+		});
+	}
+}
+function equalHeight() {
+	$('.js-item').matchHeight();
+}
+
+function slider() {
+	var mySwiper = new Swiper('.js-slider', {
+		// Optional parameters
+		loop: true,
+	  
+		// If we need pagination
+		pagination: {
+		  el: '.swiper-pagination',
+		},
+	  
+		// Navigation arrows
+		navigation: {
+		  nextEl: '.swiper-button-next',
+		  prevEl: '.swiper-button-prev',
+		},
+	  
+		// And if we need scrollbar
+		scrollbar: {
+		  el: '.swiper-scrollbar',
+		},
+	});
+}
 function navBurger() {
     var $navBtn = $('.js-nav-toggle');
     var $nav = $('.js-nav');
@@ -16,4 +53,7 @@ function navBurger() {
     });
 }
 
+objectFitPolyfill();
+equalHeight();
+slider();
 navBurger();
